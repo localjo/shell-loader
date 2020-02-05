2ca1ce055f8d840183838f7bd26134c5b82984c8
Adam Pelle <adam.pelle@bitrise.io>
2020-02-05 09:56:09 +0100

    exposing exec options to the consumer

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const { exec } = require('child_process');
 const loaderUtils = require('loader-utils');
+const omit = require('lodash.omit');
 
 module.exports = function(source) {
-    var callback = this.async();
-    var options = loaderUtils.getOptions(this);
-    var command = exec(options.script, function(err, result) {
+    const options = loaderUtils.getOptions(this);
+    const callback = this.async();
+    const scriptOptions = omit(options, 'script');
+
+    const command = exec(options.script, scriptOptions, function(err, result) {
         if (err) return callback(err);
         callback(null, result);
     });
+
     command.stdin.write(source);
     command.stdin.end();
 };
