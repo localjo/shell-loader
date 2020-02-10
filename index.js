7c5dc62f4d1f370c2bb65d49b3551c7bc12b6544
Adam Pelle <adam.pelle@bitrise.io>
2020-02-10 21:32:36 +0100

    swap lines and remove white spaces

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const { exec } = require('child_process');
 const loaderUtils = require('loader-utils');
 
 module.exports = function(source) {
-    const { script, ...options } = loaderUtils.getOptions(this);
     const callback = this.async();
-
+    const { script, ...options } = loaderUtils.getOptions(this);
     const command = exec(script, options, function(err, result) {
         if (err) return callback(err);
         callback(null, result);
     });
-
     command.stdin.write(source);
     command.stdin.end();
 };
