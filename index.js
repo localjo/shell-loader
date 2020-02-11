c36facd858ae7635248d8dc9a19a3fbf64da74a9
Adam Pelle <adam.pelle.peter@gmail.com>
2020-02-11 11:04:22 +0100

    Expose node exec options (#1)
    Exposed exec option via webpack loader option object. Use case: pass maxBuffer property to exec call in order to handle very large input files.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const { exec } = require('child_process');
 const loaderUtils = require('loader-utils');
 
 module.exports = function(source) {
-    var callback = this.async();
-    var options = loaderUtils.getOptions(this);
-    var command = exec(options.script, function(err, result) {
+    const callback = this.async();
+    const { script, ...options } = loaderUtils.getOptions(this);
+    const command = exec(script, options, function(err, result) {
         if (err) return callback(err);
         callback(null, result);
     });
