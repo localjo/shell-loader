2e0c521a8cf7e9b8a37345132442f8830b565055
Oleg Andreyev <oleg.andreyev@ecentria.com>
2020-02-24 16:48:00 +0200

    inheriting from webpack context if it's not defined + parseInt for maxBuffer

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const loaderUtils = require('loader-utils');
 module.exports = function(source) {
     const callback = this.async();
     const { script, ...options } = loaderUtils.getOptions(this);
+
+    if (!options.cwd) {
+        options.cwd = this.context;
+    }
+
+    if (options.maxBuffer) {
+        options.maxBuffer = parseInt(options.maxBuffer);
+    }
+
     const command = exec(script, options, function(err, result) {
         if (err) return callback(err);
         callback(null, result);
